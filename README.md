84ab3ff0030053e22999a239c34362fca8a45de1
Garry Pettet <contact@garrypettet.com>
2019-02-19 13:03:56 +0000

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # xojo-rainbow
 Plugin and themes to allow the RainbowJS syntax highlighter to work with Xojo code
+
+Rainbow is an excellent browser-based Javascript syntax highlighting engine. This repo contains a minified copy of the main library (`rainbow.min.js`) and an additional file (`xojo.js`) that contains the definitions required to highlight Xojo code. Additionally, I have provided CSS and SASS files to style the highlighted code. There are both light and dark themes.
+
+## Usage
+Include the Rainbow script and Xojo definitions before the closing `</body>` tag:
+
+```html
+<script src="js/rainbow.min.js"></script>
+<script src="js/xojo.js"></script> 
+```
+
+Within the `<head>` of the page, include the CSS (light or dark, whatever you prefer):
+
+```html
+<link rel="stylesheet" href="css/rainbow-xojo-light.css">
+```
---
+++ js/xojo.js
@@ -0,0 +1,46 @@
+/**
+ * Xojo language patterns
+ *
+ * @author Dr Garry Pettet
+ */
+Rainbow.extend('xojo', [
+    {
+        name: 'comment',
+        pattern: /(\/\/|rem|').*$/igm
+    },
+
+    {
+        name: 'operator',
+        pattern: /&(gt|lt);|[+-/*=<>^]/g
+    },
+
+    // Numbers
+    {
+        name: 'number.double',
+        pattern: /\b\d+\.\d*([e][-+]*\d+)?\b/gi
+    },
+    {
+        name: 'number.integer',
+        pattern: /\b(\d+\.?\d*([e][-+]*\d+)?)|((&amp;)h[A-Fa-f0-9]+)\b|((&amp;)b[01]+)\b|((&amp;)o[0-7]+)\b/gi
+    },
+
+    // Types and keywords.
+    {
+        name: 'keyword.type',
+        pattern: /\b(Boolean|True|False|String|Text|Integer|Double|Int8|Int16|Int32|Int64|UInt8|UInt16|UInt32|UInt64)\b/gi
+    },
+    {
+        name: 'keyword',
+        pattern: /\b(AddHandler|AddressOf|And|Array|As|Assigns|Break|ByRef|ByVal|Call|Case|Catch|Class|Const|Continue|CType|Declare|Delegate|Dim|Do|DownTo|Each|Else|ElseIf|End|Enum|Event|Exception|Exit|Extends|Finally|For|Function|Global|GoTo|If|Implements|In|Inherits|Interface|Is|IsA|Lib|Loop|Me|Mod|Module|New|Next|Nil|Not|Object|Optional|Or|ParamArray|Private|Property|Protected|Public|Raise|RaiseEvent|Redim|RemoveHandler|Return|Select|Self|Shared|Soft|Static|Step|Structure|Sub|Super|Then|To|Try|Until|Using|WeakAddressOf|Wend|While|Xor)\b/gi
+    },
+    {
+        name: 'string',
+        pattern: /".*"/g
+    },
+
+    // Compiler directives.
+    {
+        name: 'directive',
+        pattern: /^#Pragma.*$/igm
+    }
+]); 
