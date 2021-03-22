f1533129b23d7f34dc86c2cdcd5e22d28d1e0ea6
XojoGermany <1276539+XojoGermany@users.noreply.github.com>
2021-03-22 15:42:01 +0100

    Xojo Update
    Added Support for "Selector", "Var" & missing Pragma Directives.

--- js/xojo.js
+++ js/xojo.js
@@ -45,16 +45,16 @@ Rainbow.extend('xojo', [
     // Types and keywords.
     {
         name: 'keyword.type',
-        pattern: /\b(Auto|Boolean|Byte|CGFloat|CFStringRef|Color|CString|Currency|Delegate|Double|False|Int8|Int16|Int32|Int64|Integer|Object|PString|Ptr|Short|Single|String|Text|True|UInt8|UInt16|UInt32|UInt64|UInteger|WindowPtr|WString)\b/gi
+        pattern: /\b(Auto|Boolean|Byte|CGFloat|CFStringRef|Color|CString|Currency|Delegate|Double|False|Object|PString|Ptr|Short|Single|String|Text|True|(?:U)?Int8|(?:U)?Int16|(?:U)?Int32|(?:U)?Int64|(?:U)?Integer|WindowPtr|WString)\b/gi
     },
     {
         name: 'keyword',
-        pattern: /\b(AddHandler|AddressOf|And|Array|As|Assigns|Break|ByRef|ByVal|Call|Case|Catch|Class|Const|Continue|CType|Declare|Dim|Do|DownTo|Each|Else|ElseIf|End|Enum|Event|Exception|Exit|Extends|Finally|For|Function|Global|GoTo|If|Implements|In|Inherits|Interface|Is|IsA|Lib|Loop|Me|Mod|Module|New|Next|Nil|Not|Optional|Or|ParamArray|Private|Property|Protected|Public|Raise|RaiseEvent|Redim|RemoveHandler|Return|Select|Self|Shared|Soft|Static|Step|Structure|Sub|Super|Then|To|Try|Until|Using|WeakAddressOf|Wend|While|Xor)\b/gi
+        pattern: /\b(AddHandler|AddressOf|And|Array|As|Assigns|Break|ByRef|ByVal|Call|Case|Catch|Class|Const|Continue|CType|Declare|Dim|Do|DownTo|Each|Else|ElseIf|End|Enum|Event|Exception|Exit|Extends|Finally|For|Function|Global|GoTo|If|Implements|In|Inherits|Interface|Is|IsA|Lib|Loop|Me|Mod|Module|New|Next|Nil|Not|Optional|Or|ParamArray|Private|Property|Protected|Public|Raise|RaiseEvent|Redim|RemoveHandler|Return|Select(?:or)?|Self|Shared|Soft|Static|Step|Structure|Sub|Super|Then|To|Try|Until|Using|Var|WeakAddressOf|Wend|While|Xor)\b/gi
     },
 
     // Compiler directives.
     {
         name: 'directive',
-        pattern: /^#Pragma.*$/igm
+        pattern: /#(?:If|Else(?:If)|End(?:If)|Pragma)/g
     }
 ]); 
